7bf7fe8d423d6c963799475884c57b5a1a7e3a58
Carter Harris <lookoutcarter@outlook.com>
2019-02-09 12:42:03 -0700

    Comments added.

--- src/InputFile.java
+++ src/InputFile.java
@@ -1,3 +1,17 @@
 public class InputFile {
     
 }
+
+
+//reading files
+//read in to string line by line
+//convert letters in to hex 
+//parse string and turn it in to an array for each letter counting the number of occurances.
+//lowercase all letters
+//Character.valueOf() gives number vaule of letter
+//once have number of occurences of each letter. create frequency analysis
+//freq[letterValue - 96] = freq.length((freq[letterValue - 96] + 1)/freq.length)
+//match the frequency with the known frequencies. 
+//use max() to find the max vaule of freq analysis and add that letter to string to create a key.
+//once have key use subsitution
+
