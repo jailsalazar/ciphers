cb9504c2347f77765c933ae5c3393613bca3c030
Carter Harris <lookoutcarter@outlook.com>
2019-02-10 09:25:16 -0700

    IC

---
+++ src/decryption/IndexCoinc.java
@@ -0,0 +1,19 @@
+package decryption;
+
+public class IndexCoinc {
+//	double num = 0.0;
+//	double den = 0.0;
+//	double IC = 0.0;
+//	
+//	den = cipher.length*(ciper.length-1);
+//	
+//	for (i = 0; i < 25; i++) {
+//		
+//		num += freqNum.value(i)(freqNum.value.(i) - 1);
+//		
+//		
+//	}
+//	
+//	IC = num/den;
+
+}
